f3f49dacb2898a930a161e04d10ab6575b966fc1
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-16 22:18:23 -0300

    [GE] Atualiza variaveis de ambiente

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,8 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-if (!process.env.API_ENDPOINT) {
+if (!process.env.REACT_APP_API_ENDPOINT) {
     throw new Error('Variável de ambiente API_ENDPOINT vazia')
 }
 
 export const env = {
-    apiEndpoint: process.env.API_ENDPOINT
+    apiEndpoint: process.env.REACT_APP_API_ENDPOINT,
+    test: process.env.REACT_APP_TEST === 'true'
 }
