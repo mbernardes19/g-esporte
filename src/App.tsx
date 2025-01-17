179c1e4e3d5f2056145f19d40e913b9e42021730
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-16 21:53:53 -0300

    [GE] Atualiza codigo para novo padrao do prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Partida } from './components/Partida/Partida'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const fetchPartida = async () => {}
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <main></main>
+        </div>
+    )
 }
 
-export default App;
+export default App
