49fcb3be7163915f2acb4a88d2b090a388372817
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-19 17:34:17 -0300

    [GE] Cria LazyWrapper para carregar componentes tardiamente

---
+++ src/components/LazyWrapper/LazyWrapper.tsx
@@ -0,0 +1,33 @@
+import React, { LazyExoticComponent, PropsWithChildren, Suspense } from 'react'
+import { useInView } from 'react-intersection-observer'
+import cn from 'classnames'
+
+type LazyWrapperProps<T extends Record<string, unknown>> = {
+    Component: LazyExoticComponent<React.ComponentType<T>>
+    fallback: JSX.Element
+    componentProps: T
+    className?: string
+} & PropsWithChildren
+
+export const LazyWrapper = <T extends Record<string, unknown>>({
+    Component,
+    fallback,
+    componentProps,
+    children,
+    className
+}: LazyWrapperProps<T>) => {
+    const { ref, inView } = useInView()
+
+    return (
+        <div className={cn('w-full', className)} ref={ref}>
+            {inView ? (
+                <Suspense fallback={fallback}>
+                    {/* @ts-expect-error Tipos não batem 100% mas não causa erros */}
+                    <Component {...componentProps}>{children}</Component>
+                </Suspense>
+            ) : (
+                fallback
+            )}
+        </div>
+    )
+}
