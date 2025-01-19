d15ac1ea354903daadcda14b7c7764e53eadb8f0
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-19 11:27:14 -0300

    [GE] Atualiza home para exibir todos os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ErrorComponent } from './components/ErrorComponent/ErrorComponent'
 import { PartidaCard } from './components/PartidaCard/PartidaCard'
 import { Section } from './components/Section/Section'
 import { usePartidaFetch } from './lib/hooks/usePartidaFetch'
+import { PartidaProvider } from '@lib/context/partidaContext'
+import { Lances } from '@components/Lances/Lances'
 
 function App() {
     const { partida, error, isLoading } = usePartidaFetch()
@@ -19,32 +21,37 @@ function App() {
     return (
         <div className="App">
             <main className="main-container">
-                <Section titulo="Partidas do dia">
-                    {partida?.JogosDia.map(
-                        (
-                            {
-                                Equipe1,
-                                Equipe2,
-                                GolsEquipe1,
-                                GolsEquipe2,
-                                Status
-                            },
-                            idx
-                        ) => (
-                            <PartidaCard
-                                key={idx}
-                                equipe1={Equipe1}
-                                equipe2={Equipe2}
-                                golsEquipe1={GolsEquipe1}
-                                golsEquipe2={GolsEquipe2}
-                                status={Status}
-                            />
-                        )
-                    )}
-                </Section>
-                <Section titulo="Partidas recentes">
-                    <PartidaSelector partidas={partida.ListaPartidas} />
-                </Section>
+                <PartidaProvider dados={partida}>
+                    <Section titulo="Partidas do dia">
+                        {partida?.JogosDia.map(
+                            (
+                                {
+                                    Equipe1,
+                                    Equipe2,
+                                    GolsEquipe1,
+                                    GolsEquipe2,
+                                    Status
+                                },
+                                idx
+                            ) => (
+                                <PartidaCard
+                                    key={idx}
+                                    equipe1={Equipe1}
+                                    equipe2={Equipe2}
+                                    golsEquipe1={GolsEquipe1}
+                                    golsEquipe2={GolsEquipe2}
+                                    status={Status}
+                                />
+                            )
+                        )}
+                    </Section>
+                    <Section titulo="Partidas recentes">
+                        <PartidaSelector partidas={partida.ListaPartidas} />
+                    </Section>
+                    <Section titulo="Lances" className="centralize">
+                        <Lances lances={partida.Lances} />
+                    </Section>
+                </PartidaProvider>
             </main>
         </div>
     )
