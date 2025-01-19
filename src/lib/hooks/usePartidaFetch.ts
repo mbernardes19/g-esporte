829aa65fdccbf533b1c7f7e62bff7c6b27d163fb
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-19 09:21:57 -0300

    [GE] Adiciona dados mockados a usePartidaFetch para retonar quando acessar outras partidas

--- src/lib/hooks/usePartidaFetch.ts
+++ src/lib/hooks/usePartidaFetch.ts
@@ -1,9 +1,27 @@
 import { DadosPartida } from '@customTypes/api'
 import { env } from '@lib/env'
 import { useFetch } from '@lib/hooks/useFetch'
+import dadosPartida20488 from '../../test/mocks/dadosPartida_20488.json'
+import dadosPartida20487 from '../../test/mocks/dadosPartida_20487.json'
+import { useNavigation } from './useNavigation'
 
 export const usePartidaFetch = () => {
     const { data, error, isLoading } = useFetch<DadosPartida>(env.apiEndpoint)
+    const { lastSegment } = useNavigation()
+
+    if (lastSegment === '20488') {
+        return {
+            partida: dadosPartida20488 as unknown as DadosPartida,
+            error: null,
+            isLoading: false
+        }
+    } else if (lastSegment === '20487') {
+        return {
+            partida: dadosPartida20487 as unknown as DadosPartida,
+            error: null,
+            isLoading: false
+        }
+    }
 
     return {
         partida: data,
