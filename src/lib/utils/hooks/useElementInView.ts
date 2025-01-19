173983d75c8582dfd73521819fdbff37d4164c3b
Matheus Bernardes <bernardes.matheus@outlook.com>
2025-01-19 15:26:22 -0300

    [GE] Cria useElementInView para identificar se algum elemento está na tela

---
+++ src/lib/utils/hooks/useElementInView.ts
@@ -0,0 +1,40 @@
+import { MutableRefObject, useEffect } from 'react'
+
+export const useElementInView = (
+    elementRefOrSelectors: (MutableRefObject<Element | null> | string)[],
+    handler: (entries?: IntersectionObserverEntry) => void
+): void => {
+    const observersList: IntersectionObserver[] = []
+
+    useEffect(() => {
+        elementRefOrSelectors?.forEach((refOrSelector) => {
+            if (typeof refOrSelector === 'string') {
+                const elements = document.querySelectorAll(refOrSelector)
+
+                elements.forEach((element) => {
+                    if (!element) return
+                    const observer = new IntersectionObserver(
+                        (entries) => handler(entries[0]),
+                        {
+                            threshold: 0.3
+                        }
+                    )
+                    observer.observe(element)
+                    observersList?.push(observer)
+                })
+            } else {
+                const element = refOrSelector?.current
+                if (!element) return
+                const observer = new IntersectionObserver((entries) =>
+                    handler(entries[0])
+                )
+                observer.observe(element)
+                observersList?.push(observer)
+            }
+        })
+
+        return () =>
+            observersList &&
+            observersList.forEach((observer) => observer.disconnect())
+    }, [JSON.stringify(elementRefOrSelectors)])
+}
